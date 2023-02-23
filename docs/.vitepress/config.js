53e5f7e052deb0e09257a3713baf8391d6acace0
戴鸿锟 <9335862+dai-hongkun@user.noreply.gitee.com>
2023-02-23 22:53:29 +0800

    update

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,11 +1,11 @@
 export default {
-  title: '戴鸿锟的个人站点', // 站点名称
+  title: '清晨投递的个人站点', // 站点名称
   description: 'Just playing around.',
-  srcDir: '/pages/', // 配置根路径
+  srcDir: './pages/', // 配置根路径
   lastUpdated: true, // 显示文章更新时间
   outDir: '/public', // 打包后保存路径
   themeConfig: { // 主题配置
-    siteTitle: '戴鸿锟的个人站点',
+    siteTitle: '清晨投递',
     lastUpdatedText: '最后更新：', // 时间前缀文字
     docFooter: { // 上一页/下一页上方显示的文字
       prev: '上一篇',
@@ -13,6 +13,7 @@ export default {
     },
     // logo: "" // logo
     nav: [ // 页面导航
+      { text: '主页', link: '/' },
       { text: '关于我', link: '/my/' },
       {
         text: '前端',
@@ -27,6 +28,12 @@ export default {
           { text: '黑马头条', link: '/articles/other/javascript/' },
           { text: '后台管理系统', link: '/articles/other/git/' }
         ]
+      },
+      {
+        text: '年终总结',
+        items: [
+          { text: '2022', link: '/year/2022/' },
+        ]
       }],
     sidebar: [ // 侧边栏
       {
@@ -35,14 +42,8 @@ export default {
         collapsed: true,
         items: [
           { text: 'JavaScript', link: '/articles/other/javascript/' },
-          // { text: 'Git', link: '/articles/other/git/' }
         ]
       },
-      {
-        text: 'Section Title B',
-        collapsible: true,
-        items: [{ text: 'Index', link: '/config/' }]
-      }
     ],
     socialLinks: [ // 社交账户
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
--- docs/pages/index.md
+++ docs/pages/index.md
@@ -2,17 +2,17 @@
 layout: home
 
 hero:
-  name: 个人站点
-  text: 个人站点
+  name: 清晨投递的个人站点
+  text: 宝剑锋从磨砺出，梅花香自苦寒来。
   tagline: 没有努力，怎么会有回报。
   image:
     src: https://cn.vitejs.dev/logo-with-shadow.png
     alt: logo
   actions:
     - theme: brand
-      text: 点击我
-      link: /guide/what-is-vitepress
+      text: 关于我
+      link: /my/
     - theme: alt
       text: 我的项目
-      link: https://github.com/vuejs/vitepress
+      link: /
 ---
