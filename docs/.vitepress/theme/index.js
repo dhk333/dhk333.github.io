2d6f563e9ebf0193b35d07c76e0aa20b50e5d7e1
15779728359 <1731381719@qq.com>
2023-03-04 17:59:05 +0800

    deploy

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,23 +1,23 @@
-// /* 自定义主题 */
+/* 自定义主题 */
 
-// // 导入默认主题
-// import DefaultTheme from 'vitepress/theme'
+// 导入默认主题
+import DefaultTheme from 'vitepress/theme'
 
-// // 导入自定义主题样式/覆盖默认样式
-// import './custom.css'
-
-// // 导出
-// export default DefaultTheme
-
-import Theme from "vitepress/theme"
-
+// 导入自定义主题样式/覆盖默认样式
 import './custom.css'
 
-import comment from "../components/git-talk.vue"
-export default {
-  ...Theme,
-  enhanceApp (ctx) {
-    Theme.enhanceApp(ctx)
-    ctx.app.component("git-talk", comment)
-  },
-}
+// 导出
+export default DefaultTheme
+
+// import Theme from "vitepress/theme"
+
+// import './custom.css'
+
+// import comment from "../components/git-talk.vue"
+// export default {
+//   ...Theme,
+//   enhanceApp (ctx) {
+//     Theme.enhanceApp(ctx)
+//     ctx.app.component("git-talk", comment)
+//   },
+// }
